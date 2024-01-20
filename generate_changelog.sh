2c1108c8377ced1e757f21fe2f73c5137778b3c2
Stephan Michard <github@michard.io>
2024-01-20 21:31:36 +0100

    feat: :sparkles: removed hard coded repository url
    github repository url is retrieved from git repository

--- generate_changelog.sh
+++ generate_changelog.sh
@@ -9,10 +9,11 @@ trap 'echo "An error occurred at line $LINENO. Exiting."' ERR
 # Path to the Git repository (current directory)
 REPO_DIR="."
 CHANGELOG_FILE="$REPO_DIR/CHANGELOG.md"
-GITHUB_REPO_URL="https://github.com/smichard/cities_demo" # Replace with your repository URL
+GITHUB_REPO_URL=$(git remote get-url origin | sed 's/\.git$//') # get repository url from git remote
 
 echo "Starting changelog generation script..."
-
+echo "Repository:"
+echo $GITHUB_REPO_URL
 # Create or clear the changelog file
 echo -n > $CHANGELOG_FILE
 
